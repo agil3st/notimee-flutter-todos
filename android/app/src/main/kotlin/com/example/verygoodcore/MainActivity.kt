dca1504152d3a7a13c936a6e8f3c81fa4007c369
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-16 10:11:40 +0700

    First init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.notimee_flutter_todos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:notimee_flutter_todos/app/app.dart';
+import 'package:notimee_flutter_todos/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
